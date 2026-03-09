0ff872a54b706694673ab791c354c44ec058af5e
Matheus Germano Pedroso Krohn <matheuskrohn@gmail.com>
2026-03-09 01:49:13 -0300

    Spring

---
+++ Aenba/src/test/java/com/example/Aenba/AenbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aenba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AenbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
